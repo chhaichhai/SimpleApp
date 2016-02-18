c0c791761006b572069eac86b9476ec73d64fa16
Chhai Eng <ceng@transplantconnect.com>
2016-02-17 19:26:32 -0800

    Renaming some pages

--- SimpleApp/Controllers/HomeController.cs
+++ SimpleApp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace SimpleApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -13,14 +14,15 @@ namespace SimpleApp.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Insurance()
         {
+
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Transplant()
         {
             ViewBag.Message = "Your contact page.";
 
